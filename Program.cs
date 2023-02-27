e7ecfbc5d2320c4415968ea823d03af29f508776
Cassie Dunning <hello.cassdunning@gmail.com>
2023-02-27 13:25:34 -0600

    phase1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Guess the secret number");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
         }
+
     }
 }
