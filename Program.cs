af537a9bff9793d9f9e0c4d5457f115abc71aa1c
Cassie Dunning <hello.cassdunning@gmail.com>
2023-02-28 10:08:47 -0600

    phase1 w/ comments

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
+            // display a message to the user, asking them to guess the secret number
             Console.WriteLine("Guess the secret number");
+            // display a prompt for the user's guess (--> ReadLine)
+            //  take the user's guess as an input and save it as a variable (-->  input)
             string input = Console.ReadLine();
+            // display the user's guess back to the screen
             Console.WriteLine(input);
         }
 
     }
 }
+
+
